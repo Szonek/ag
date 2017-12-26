174d9704951dcc93b835f70b4f86b5bcbeae0503
Marek <marek1cz@gmail.com>
2017-12-26 23:31:21 +0100

    Example of using functions

---
+++ functions_example.py
@@ -0,0 +1,14 @@
+from functions import Goldstein_Price
+from functions import Hartman_1
+from functions import Hartman_2
+from functions import Shekel_1
+from functions import Shekel_2
+
+
+
+
+print(Goldstein_Price([0,-1]))
+print(Hartman_1([0.114,0.556,0.852]))
+print(Hartman_2([0.201,0.150,0.477,0.275,0.311,0.657]))
+print(Shekel_1([4,4,4,4]))
+print(Shekel_2([4,4,4,4]))
