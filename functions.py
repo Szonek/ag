af7c4f27322ea21c3b20644c1547e8418ed33e19
Marek <marek1cz@gmail.com>
2017-12-27 18:49:23 +0100

    Change table->arr_val

--- functions.py
+++ functions.py
@@ -8,43 +8,43 @@ def fun1(x):
     return result
 
 
-def Goldstein_Price(table):
-    def _job1(table):
-        return 1 + (table[0] + table[1] + 1) ** 2 * (
-            19 - 14 * table[0] + 3 * table[0] ** 2 - 14 * table[1] + 6 * table[0] * table[1] + 3 * table[1] ** 2)
+def Goldstein_Price(arr_val):
+    def _job1(arr_val):
+        return 1 + (arr_val[0] + arr_val[1] + 1) ** 2 * (
+            19 - 14 * arr_val[0] + 3 * arr_val[0] ** 2 - 14 * arr_val[1] + 6 * arr_val[0] * arr_val[1] + 3 * arr_val[1] ** 2)
 
 
-    def _job2(table):
-        return 30 + (2 * table[0] - 3 * table[1]) ** 2 * (
-            18 - 32 * table[0] + 12 * table[0] ** 2 + 48 * table[1] - 36 * table[0] * table[1] + 27 * table[1] ** 2)
+    def _job2(arr_val):
+        return 30 + (2 * arr_val[0] - 3 * arr_val[1]) ** 2 * (
+            18 - 32 * arr_val[0] + 12 * arr_val[0] ** 2 + 48 * arr_val[1] - 36 * arr_val[0] * arr_val[1] + 27 * arr_val[1] ** 2)
 
-    return _job1(table) * _job2(table)
+    return _job1(arr_val) * _job2(arr_val)
 
 
 
-def Hartman_1(table):
-    factors_table = [{'a1': 3, 'a2': 10, 'a3': 30, 'c': 1, 'p1': 0.3689, 'p2': 0.1170, 'p3': 0.2673},
+def Hartman_1(arr_val):
+    factors_arr_val = [{'a1': 3, 'a2': 10, 'a3': 30, 'c': 1, 'p1': 0.3689, 'p2': 0.1170, 'p3': 0.2673},
                      {'a1': 0.1, 'a2': 10, 'a3': 35, 'c': 1.2, 'p1': 0.4699, 'p2': 0.4387, 'p3': 0.7470},
                      {'a1': 3, 'a2': 10, 'a3': 30, 'c': 3, 'p1': 0.1091, 'p2': 0.8732, 'p3': 0.5547},
                      {'a1': 0.1, 'a2': 10, 'a3': 35, 'c': 3.2, 'p1': 0.03815, 'p2': 0.5743, 'p3': 0.8828}]
 
-    def _job1(table,wfactor):
+    def _job1(arr_val,wfactor):
         temp_sum = 0
-        temp_sum= temp_sum + factors_table[wfactor]['a1'] * math.pow((table[0] - factors_table[wfactor]['p1']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a2'] * math.pow((table[1] - factors_table[wfactor]['p2']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a3'] * math.pow((table[2] - factors_table[wfactor]['p3']), 2)
+        temp_sum= temp_sum + factors_arr_val[wfactor]['a1'] * math.pow((arr_val[0] - factors_arr_val[wfactor]['p1']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a2'] * math.pow((arr_val[1] - factors_arr_val[wfactor]['p2']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a3'] * math.pow((arr_val[2] - factors_arr_val[wfactor]['p3']), 2)
         return math.exp(-temp_sum)
 
 
     sum_temp = 0
-    for x in range(len(factors_table)):
-        sum_temp = sum_temp + factors_table[x]['c'] * _job1(table, x)
+    for x in range(len(factors_arr_val)):
+        sum_temp = sum_temp + factors_arr_val[x]['c'] * _job1(arr_val, x)
     return sum_temp * -1
 
 
 
-def Hartman_2(table):
-    factors_table = [{'a1': 10, 'a2': 3, 'a3': 17, 'a4': 3.5, 'a5': 1.7, 'a6': 8, 'c': 1, 'p1': 0.1313, 'p2': 0.1696,
+def Hartman_2(arr_val):
+    factors_arr_val = [{'a1': 10, 'a2': 3, 'a3': 17, 'a4': 3.5, 'a5': 1.7, 'a6': 8, 'c': 1, 'p1': 0.1313, 'p2': 0.1696,
                     'p3': 0.5569,'p4': 0.0124, 'p5': 0.8283, 'p6': 0.5886},
                      {'a1': 0.1, 'a2': 0.05, 'a3': 17, 'a4': 0.1, 'a5': 8, 'a6': 14, 'c': 1.2, 'p1': 0.2329,'p2': 0.4135,
                       'p3': 0.8307,'p4': 0.3736, 'p5': 0.1004, 'p6': 0.9991},
@@ -54,46 +54,46 @@ def Hartman_2(table):
                       'p3': 0.8732,'p4': 0.5743, 'p5': 0.1091, 'p6': 0.0381}
                      ]
 
-    def _job1(table,wfactor):
+    def _job1(arr_val,wfactor):
         temp_sum = 0
-        temp_sum= temp_sum + factors_table[wfactor]['a1'] * math.pow(
-            (table[0] - factors_table[wfactor]['p1']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a2'] * math.pow(
-            (table[1] - factors_table[wfactor]['p2']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a3'] * math.pow(
-            (table[2] - factors_table[wfactor]['p3']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a4'] * math.pow(
-            (table[3] - factors_table[wfactor]['p4']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a5'] * math.pow(
-            (table[4] - factors_table[wfactor]['p5']), 2)
-        temp_sum = temp_sum + factors_table[wfactor]['a6'] * math.pow(
-            (table[5] - factors_table[wfactor]['p6']), 2)
+        temp_sum= temp_sum + factors_arr_val[wfactor]['a1'] * math.pow(
+            (arr_val[0] - factors_arr_val[wfactor]['p1']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a2'] * math.pow(
+            (arr_val[1] - factors_arr_val[wfactor]['p2']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a3'] * math.pow(
+            (arr_val[2] - factors_arr_val[wfactor]['p3']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a4'] * math.pow(
+            (arr_val[3] - factors_arr_val[wfactor]['p4']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a5'] * math.pow(
+            (arr_val[4] - factors_arr_val[wfactor]['p5']), 2)
+        temp_sum = temp_sum + factors_arr_val[wfactor]['a6'] * math.pow(
+            (arr_val[5] - factors_arr_val[wfactor]['p6']), 2)
         return math.exp(-temp_sum)
     sum_temp = 0
-    for x in range(len(factors_table)):
-        sum_temp = sum_temp + factors_table[x]['c'] * _job1(table, x)
+    for x in range(len(factors_arr_val)):
+        sum_temp = sum_temp + factors_arr_val[x]['c'] * _job1(arr_val, x)
     return sum_temp * -1
 
 
 
 
-def Shekel_1(table):
-    factors_table = [{'a1': 4, 'a2': 4, 'a3': 4, 'a4': 4, 'c': 0.1},
+def Shekel_1(arr_val):
+    factors_arr_val = [{'a1': 4, 'a2': 4, 'a3': 4, 'a4': 4, 'c': 0.1},
                      {'a1': 1, 'a2': 1, 'a3': 1, 'a4': 1, 'c': 0.2},
                      {'a1': 8, 'a2': 8, 'a3': 8, 'a4': 8, 'c': 0.2},
                      {'a1': 6, 'a2': 6, 'a3': 6, 'a4': 6, 'c': 0.4},
                      {'a1': 3, 'a2': 7, 'a3': 3, 'a4': 7, 'c': 0.6}]
 
     sum_temp = 0
-    for x in range(len(factors_table)):
-        sum_temp = sum_temp + _shekel_job1(table, x,factors_table)
+    for x in range(len(factors_arr_val)):
+        sum_temp = sum_temp + _shekel_job1(arr_val, x,factors_arr_val)
 
     return sum_temp * -1
 
 
 
-def Shekel_2(table):
-    factors_table = [{'a1': 4, 'a2': 4, 'a3': 4, 'a4': 4, 'c': 0.1},
+def Shekel_2(arr_val):
+    factors_arr_val = [{'a1': 4, 'a2': 4, 'a3': 4, 'a4': 4, 'c': 0.1},
                      {'a1': 1, 'a2': 1, 'a3': 1, 'a4': 1, 'c': 0.2},
                      {'a1': 8, 'a2': 8, 'a3': 8, 'a4': 8, 'c': 0.2},
                      {'a1': 6, 'a2': 6, 'a3': 6, 'a4': 6, 'c': 0.4},
@@ -103,17 +103,17 @@ def Shekel_2(table):
 
 
     sum_temp = 0
-    for x in range(len(factors_table)):
-        sum_temp = sum_temp + _shekel_job1(table, x,factors_table)
+    for x in range(len(factors_arr_val)):
+        sum_temp = sum_temp + _shekel_job1(arr_val, x,factors_arr_val)
 
     return sum_temp * -1
 
 
 
-def _shekel_job1(table,wfactor,factors_table):
+def _shekel_job1(arr_val,wfactor,factors_arr_val):
     temp_sum = 0
-    temp_sum= temp_sum + math.pow((table[0] - factors_table[wfactor]['a1']), 2)
-    temp_sum= temp_sum + math.pow((table[1] - factors_table[wfactor]['a2']), 2)
-    temp_sum= temp_sum + math.pow((table[2] - factors_table[wfactor]['a3']), 2)
-    temp_sum= temp_sum + math.pow((table[3] - factors_table[wfactor]['a4']), 2) + factors_table[wfactor]['c']
+    temp_sum= temp_sum + math.pow((arr_val[0] - factors_arr_val[wfactor]['a1']), 2)
+    temp_sum= temp_sum + math.pow((arr_val[1] - factors_arr_val[wfactor]['a2']), 2)
+    temp_sum= temp_sum + math.pow((arr_val[2] - factors_arr_val[wfactor]['a3']), 2)
+    temp_sum= temp_sum + math.pow((arr_val[3] - factors_arr_val[wfactor]['a4']), 2) + factors_arr_val[wfactor]['c']
     return math.pow(temp_sum,-1)
